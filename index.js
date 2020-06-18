2a5df9be00462ed0f1073af6346a09916f73e5db
Cantesani <cantesani@hotmail.com>
2020-06-18 13:02:56 -0300

    update
    Aula 01
    Desafio 01

---
+++ index.js
@@ -0,0 +1,15 @@
+// criar um programa que calcula a média
+// das notas entre os alunos e envia
+// mensagem do calculo da média
+// Calcular se a media é Maior ou menor que 5
+
+
+const nome = "Gabriel";
+const peso = 78;
+const altura = 1.76;
+const imc = peso / (altura * altura);
+
+if (imc >= 30) {
+    console.log (`${nome}, seu IMC é ${imc} e você está acima do peso`)
+}else 
+console.log (`${nome}, seu IMC é ${imc} e você não está acima do peso`)
