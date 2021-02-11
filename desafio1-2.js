36025d874d4f3317d6446a0cd8c710bd68d8d16b
Cantesani <cantesani@hotmail.com>
2021-02-11 19:41:32 -0300

    Atualização

---
+++ desafio1-2.js
@@ -0,0 +1,14 @@
+const empresa = 
+{
+    nome: 'RocketSeat',
+    cor: 'Roxo',
+    foco: 'Programação',
+    endereco: 
+    {
+        rua: 'Dr Gentil Leite Martins',
+        numero: '152'
+        
+    } 
+}
+
+console.log(`A empresa ${empresa.nome}, fica situada no endereço: ${empresa.endereco.rua} N. ${empresa.endereco.numero} `)
--- index.js
+++ index.js
@@ -1,15 +1,65 @@
-// criar um programa que calcula a média
-// das notas entre os alunos e envia
-// mensagem do calculo da média
-// Calcular se a media é Maior ou menor que 5
 
+const alunosDaTurmaA = [
+    {
+        nome: 'Gabriel',
+        nota: 10
+    },
+    {
+        nome: 'Zullino',
+        nota: 10
+    },
+    {
+        nome: 'Bocheca',
+        nota: 5
+    },
+    {
+        nome: 'josé',
+        nota: 5
+    }
+]
+const alunosDaTurmaB = [
+    {
+        nome: 'Ricardo',
+        nota: 4.9
+    },
+    {
+        nome: 'Duda',
+        nota: 5
+    },
+    {
+        nome: 'Felipe',
+        nota: 5
+    },
+    {
+        nome: 'Joao',
+        nota: 5
+    },
+    {
+        nome: 'Mario',
+        nota: 9
+    } 
 
-const nome = "Gabriel";
-const peso = 78;
-const altura = 1.76;
-const imc = peso / (altura * altura);
+]
 
-if (imc >= 30) {
-    console.log (`${nome}, seu IMC é ${imc} e você está acima do peso`)
-}else 
-console.log (`${nome}, seu IMC é ${imc} e você não está acima do peso`)
+function calculaMedia(alunos) {
+    let soma = 0; 
+    for (i = 0; i < alunos.length; i++) {
+        soma = soma + alunos[i].nota
+    }
+    const media = soma / alunos.length
+    return media
+
+}
+
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+function enviaMensagem(media, turma) {
+   if (media >= 5) {
+    console.log (`Parabéns, a Media da ${turma} foi ${media}`)
+}  else {
+    console.log (`Infelizmente a media da ${turma} foi ${media}`) 
+}
+} 
+enviaMensagem(media1, 'turma A')
+enviaMensagem(media2, 'turma B', 'sala 2')
