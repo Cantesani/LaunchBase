636577821a598b36759c09f6555ee5030d745c53
Cantesani <cantesani@hotmail.com>
2021-03-17 21:52:22 -0300

    Atualização de parametros.

---
+++ aula-Boleano.js
@@ -0,0 +1,95 @@
+const alunosDaTurmaA = [{
+        nome: 'Gabriel',
+        nota: 9
+    },
+    {
+        nome: 'Mayara',
+        nota: 9
+    },
+    {
+        nome: 'Sasha',
+        nota: 9
+    },
+    {
+        nome: 'josé',
+        nota: 2
+    },
+    {
+        nome: 'Sasha',
+        nota: 10
+    }
+]
+
+const alunosDaTurmaB = [{
+        nome: 'Renato',
+        nota: 10
+    },
+    {
+        nome: 'Flavia',
+        nota: 2
+    },
+    {
+        nome: 'Fernando',
+        nota: 10
+    },
+    {
+        nome: 'Fernando',
+        nota: 10
+    },
+    {
+        nome: 'Fernando',
+        nota: 10
+    }
+]
+
+function calculaMedia(alunos) {
+    let soma = 0;
+    for (let i = 0; i < alunos.length; i++) {
+        soma = soma + alunos[i].nota
+    }
+    const media = soma / alunos.length
+    return media
+}
+
+
+
+function enviaMensagem(media, turma) {
+    if (media >= 5) {
+        console.log(`Parabens, a media da ${turma} foi maior que 5. Media: ${media}`)
+    } else {
+        console.log(`Infelizmente a media foi menor que 5. Media: ${media}`)
+    }
+}
+
+
+
+function marcarComoReprovado(aluno) {
+    aluno.reprovado = false;
+    if (aluno.nota < 5) {
+        aluno.reprovado = true
+
+    }
+}
+
+function enviarMensagemReprovado(aluno) {
+    if (aluno.reprovado) {
+        console.log(`O Aluno ${aluno.nome} está reprovado`)
+
+    }
+}
+
+function alunosReprovados(alunos) {
+    for (let aluno of alunos) {
+        marcarComoReprovado(aluno);
+        enviarMensagemReprovado(aluno);
+    }
+}
+
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+enviaMensagem(media1, 'Turma A')
+enviaMensagem(media2, 'Turma B')
+
+alunosReprovados(alunosDaTurmaA)
+alunosReprovados(alunosDaTurmaB)
--- index.js
+++ index.js
@@ -1,77 +1,91 @@
-// criar um programa que calcula a média
-// Das notas entre os alunos e envia
-// mensagem do calculo da media
-// se a media for maior que 5, parabelizar a turma
-
-
-const alunosDaTurmaA = [ 
-    {
-        nome: 'Gabriel',
-        nota: 9
-    },
-    {
-        nome: 'Mayara',
-        nota: 9
-    },
-    {
-        nome: 'Sasha',
-        nota: 9
-    },
-    {
-        nome: 'josé',
-        nota: 10
-    },
-    {
-        nome: 'Sasha',
-        nota: 10
-    }
-
-]
-const alunosDaTurmaB = [ 
-    {
-        nome: 'Renato',
-        nota: 10
-    },
-    {
-        nome: 'Flavia',
-        nota: 10
-    },
-    {
-        nome: 'Fernando',
-        nota: 10
-    },
-    {
-        nome: 'Fernando',
-        nota: 10
-    },
-    {
-        nome: 'Fernando',
-        nota: 10
-    }
-
+const alunosDaTurmaA = [{
+    nome: 'Gabriel',
+    nota: 9
+},
+{
+    nome: 'Mayara',
+    nota: 9
+},
+{
+    nome: 'Sasha',
+    nota: 9
+},
+{
+    nome: 'josé',
+    nota: 2
+},
+{
+    nome: 'Lucas',
+    nota: 10
+}
 ]
 
-function calculaMedia(alunos) { 
-    let soma = 0;
-    for (let i=0; i < alunos.length; i++ ) {
-        soma = soma + alunos[i].nota
-    }
-    const media = soma / alunos.length
-    return media
+const alunosDaTurmaB = [{
+    nome: 'Renato',
+    nota: 10
+},
+{
+    nome: 'Maria',
+    nota: 2
+},
+{
+    nome: 'Fernando',
+    nota: 10
+},
+{
+    nome: 'Joaquim',
+    nota: 4.5
+},
+{
+    nome: 'Manoel',
+    nota: 10
+}
+]
 
+function calculaMedia(alunos) {
+let soma = 0;
+for (let i = 0; i < alunos.length; i++) {
+    soma = soma + alunos[i].nota
+}
+const media = soma / alunos.length
+return media
+}
+
+function enviaMensagem(media, turma) {
+if (media >= 5) {
+    console.log(`Parabens, a media da ${turma} foi maior que 5. Media: ${media}`)
+} else {
+    console.log(`Infelizmente a media foi menor que 5. Media: ${media}`)
+}
+}
+
+function marcarComoReprovado(aluno) {
+aluno.reprovado = false;
+if (aluno.nota < 5) {
+    aluno.reprovado = true
+
+}
+}
+
+function enviarMensagemReprovado(aluno) {
+if (aluno.reprovado) {
+    console.log(`O Aluno ${aluno.nome} está reprovado`)
+
+}
+}
+
+function alunosReprovados(alunos) {
+for (let aluno of alunos) {
+    marcarComoReprovado(aluno);
+    enviarMensagemReprovado(aluno);
+}
 }
 
 const media1 = calculaMedia(alunosDaTurmaA)
 const media2 = calculaMedia(alunosDaTurmaB)
 
-function enviaMensagem(media, turma) {
-    if (media>= 5)
-    { 
-        console.log (`Parabens, a media da ${turma} foi maior que 5. Media: ${media}`)
-    } else {
-        console.log (`Infelizmente a media foi menor que 5. Media: ${media}`)
-    }
-    }   
+enviaMensagem(media1, 'Turma A')
+enviaMensagem(media2, 'Turma B')
 
-    enviaMensagem(media1, 'Turma A')
-    enviaMensagem(media2, 'Turma B')
+alunosReprovados(alunosDaTurmaA)
+alunosReprovados(alunosDaTurmaB)
